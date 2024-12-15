3273bd634c6f44ffb1d079847aaeaf8601e0a810
mthierman <mthierman@gmail.com>
2024-12-14 19:34:21 -0700

    Update default bin

--- sill/src/bin/main.rs
+++ sill/src/bin/main.rs
@@ -1,8 +1,37 @@
-use sill::*;
+// #![windows_subsystem = "windows"]
 
-fn main() {
-    message_box(
-        &format!("🪟 {}", env!("CARGO_PKG_NAME")),
-        &format!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")),
-    );
+use sill::{
+    message_loop, quit,
+    window::{default_procedure, WindowBuilder, WindowEventHandler},
+};
+use std::{process::ExitCode, rc::Rc};
+use windows::Win32::{
+    Foundation::LRESULT,
+    UI::WindowsAndMessaging::{WM_CREATE, WM_DESTROY, WS_OVERLAPPEDWINDOW},
+};
+
+fn main() -> ExitCode {
+    let events: WindowEventHandler = Rc::new(move |_window, event| match event.msg {
+        WM_CREATE => {
+            println!("WM_CREATE");
+
+            LRESULT(1)
+        }
+        WM_DESTROY => {
+            println!("WM_DESTROY");
+
+            quit(0);
+
+            LRESULT(0)
+        }
+        _ => default_procedure(event),
+    });
+
+    let _window = WindowBuilder::new()
+        .title("Sill")
+        .style(WS_OVERLAPPEDWINDOW)
+        .events(events)
+        .create(true);
+
+    message_loop()
 }
