3a4b83480416f63cb2cff6ce17a45ab917f876a2
mthierman <mthierman@gmail.com>
2024-12-16 16:33:57 -0700

    Insert and return id

--- sill/src/window/manager.rs
+++ sill/src/window/manager.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use windows::Win32::{
     Foundation::HWND,
     UI::WindowsAndMessaging::{WINDOW_EX_STYLE, WINDOW_STYLE},
@@ -8,6 +10,7 @@ use super::{Window, WindowBuilder, WindowEventHandler};
 #[derive(Default)]
 pub struct WindowManager {
     builder: WindowBuilder,
+    map: HashMap<usize, Box<Window>>,
 }
 
 impl WindowManager {
@@ -51,15 +54,33 @@ impl WindowManager {
         self
     }
 
-    pub fn add(&self) -> Box<Window> {
-        self.builder.build()
+    pub fn add(&mut self) -> usize {
+        let window = self.builder.build();
+
+        let id = self.map.len() + 1;
+
+        self.map.insert(id, window);
+
+        id
     }
 
-    pub fn add_child(&self, parent: HWND) -> Box<Window> {
-        self.builder.build_child(parent)
+    pub fn add_child(&mut self, parent: HWND) -> usize {
+        let window = self.builder.build_child(parent);
+
+        let id = self.map.len() + 1;
+
+        self.map.insert(id, window);
+
+        id
     }
 
-    pub fn add_message_only(&self) -> Box<Window> {
-        self.builder.build_message_only()
+    pub fn add_message_only(&mut self) -> usize {
+        let window = self.builder.build_message_only();
+
+        let id = self.map.len() + 1;
+
+        self.map.insert(id, window);
+
+        id
     }
 }
