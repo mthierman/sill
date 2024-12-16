9268acd3b1e6734daaf883f9749d08799092a51d
mthierman <mthierman@gmail.com>
2024-12-16 16:07:06 -0700

    Add manager example

--- sill/src/window/manager.rs
+++ sill/src/window/manager.rs
@@ -5,14 +5,6 @@ pub struct WindowManager {
     pub builder: WindowBuilder,
 }
 
-// impl Default for WindowManager {
-//     fn default() -> Self {
-//         Self {
-//             builder: WindowBuilder::default(),
-//         }
-//     }
-// }
-
 impl WindowManager {
     pub fn new() -> Self {
         Default::default()
