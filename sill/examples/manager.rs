ac4842eb6b49a4b3ce5fd4a4fff93e1042f68842
mthierman <mthierman@gmail.com>
2024-12-16 16:19:52 -0700

    Add Manager methods

--- sill/examples/manager.rs
+++ sill/examples/manager.rs
@@ -4,7 +4,7 @@ use sill::{message_loop, Window};
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
-    let manager = Window::manager();
+    let manager = Window::manager().title("Test");
 
     let _window = manager.spawn();
 
--- sill/src/window/manager.rs
+++ sill/src/window/manager.rs
@@ -1,4 +1,6 @@
-use super::{Window, WindowBuilder};
+use windows::Win32::UI::WindowsAndMessaging::{WINDOW_EX_STYLE, WINDOW_STYLE};
+
+use super::{Window, WindowBuilder, WindowEventHandler};
 
 #[derive(Default)]
 pub struct WindowManager {
@@ -8,6 +10,48 @@ pub struct WindowManager {
 impl WindowManager {
     pub fn new() -> Self {
         Default::default()
+    }
+
+    pub fn with_builder(mut self, builder: WindowBuilder) -> Self {
+        self.builder = builder;
+
+        self
+    }
+
+    pub fn title(mut self, title: &str) -> Self {
+        self.builder.attributes.title = Some(title.to_owned());
+
+        self
+    }
+
+    pub fn id(mut self, id: usize) -> Self {
+        self.builder.attributes.id = Some(id);
+
+        self
+    }
+
+    pub fn style(mut self, style: WINDOW_STYLE) -> Self {
+        self.builder.attributes.style = Some(style);
+
+        self
+    }
+
+    pub fn ex_style(mut self, ex_style: WINDOW_EX_STYLE) -> Self {
+        self.builder.attributes.ex_style = Some(ex_style);
+
+        self
+    }
+
+    pub fn hidden(mut self) -> Self {
+        self.builder.attributes.hidden = true;
+
+        self
+    }
+
+    pub fn events(mut self, events: WindowEventHandler) -> Self {
+        self.builder.events = Some(events.clone());
+
+        self
     }
 
     pub fn spawn(&self) -> Box<Window> {
